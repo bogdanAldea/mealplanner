f919dd8e2bb20966162cf16f1c13fa89b1d8b354
bogdan <bogdan.aldea0623@gmail.com>
2022-01-27 13:35:47 +0200

    created & registered the ingredient model

--- ingredient/models.py
+++ ingredient/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class IngredientCategory(models.TextChoices):
+    """Class defines a TextChoice enum object that holds
+    string values which represent categories that ingredient
+    objects fall under."""
+
+    MEAT: str = "Meat"
+    VEGETABLE: str = "Vegetable"
+    FRUIT: str = "Fruit"
+    DAIRY: str = "Dairy"
+    FLOUR: str = "Flour"
+    SEASONING: str = "Seasoning"
+    OTHER: str = "Other"
+
+
+class Ingredient(models.Model):
+    """Class defines the model that represents an ingredient object."""
+
+    name: str = models.CharField(max_length=50, unique=True)
+    category: str = models.CharField(max_length=20, choices=IngredientCategory.choices)
+
+
+class AbstractComponent(models.Model):
+    """Class defines an abstract model that will allow other models
+    to inherit from without having to instantiate or create database
+    record of this parent class."""
+
+    ingredient: Ingredient
+    quantity: int
+
+    class Meta:
+        abstract = True
