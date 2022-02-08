3f6bfd7d6b614cfc1b8c3b3f73871b6e2ce05923
bogdan <bogdan.aldea0623@gmail.com>
2022-02-08 07:19:59 +0200

    addeddocstring to forms

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -2,12 +2,23 @@ from django.forms import ModelForm
 from .models import Recipe
 
 
-class CreateRecipeForm(ModelForm):
-    """Model form that handles the creation of a new recipe object.
-    After creation the object will be added automatically to user's cookbook."""
+class BaseRecipeForm(ModelForm):
+    """Base form class that handles the creation of a form that allows
+    users to manipulate recipe model objects. This form will be subclassed
+    by other child forms."""
 
     class Meta:
         model = Recipe
         fields = (
             "name", "category", "cooking_time"
         )
+
+
+class CreateRecipeForm(BaseRecipeForm):
+    """Form inherits form the BaseRecipeForm in order to create a new form
+    that will be used by views to create a new recipe object."""
+
+
+class UpdateRecipeForm(BaseRecipeForm):
+    """Form inherits from the BaseRecipeForm in order to create a new form
+    that will be used by views to update existing recipe objects."""
