ba9fcaebe4d3b31444444e6b6b10f78eff43e71c
bogdan <bogdan.aldea0623@gmail.com>
2022-02-08 07:22:43 +0200

    added urls to update views for cookbook views

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views.create import (cookbook_view, add_recipe_to_cookbook,
-                           add_instructions_to_recipe_view, add_quantified_ingredient_to_recipe)
+from .views.create_views import add_recipe_to_cookbook, add_instructions, add_quantified_ingredients
+from .views.list_views import cookbook_view
+from .views.update_views import update_recipe_view, update_instructions, update_quantified_ingredients
 
 app_name = 'cookbook'
 
 urlpatterns: list = [
     path("", cookbook_view.CookbookView, name="cookbook"),
     path("add-new-recipe/", add_recipe_to_cookbook.AddRecipeToCookbookView, name="add-new-recipe"),
-    path("add-instructions/", add_instructions_to_recipe_view.AddInstructionToRecipeView, name="add-instructions"),
-    path("add-ingredients/", add_quantified_ingredient_to_recipe.AddQuantifiedIngredientToRecipe, name="add-ingredients"),
+    path("add-instructions/", add_instructions.AddInstructionToRecipeView, name="add-instructions"),
+    path("add-ingredients/", add_quantified_ingredients.AddQuantifiedIngredientToRecipeView, name="add-ingredients"),
+
+    path("update-recipe/<int:pk>/", update_recipe_view.UpdateRecipeView, name="update-recipe"),
+    path("update-instructions/<int:pk>/", update_instructions.UpdateRecipeInstructionsView, name="update-instructions"),
+    path("update-ingredients/<int:pk>/", update_quantified_ingredients.UpdateQuantifiedIngredientsView, name="update-ingredients"),
 ]
