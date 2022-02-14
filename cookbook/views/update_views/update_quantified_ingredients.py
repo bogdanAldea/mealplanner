84f0cbd44020abf18455d09a7917c1c4cbf90642
bogdan <bogdan.aldea0623@gmail.com>
2022-02-14 10:49:04 +0200

    cleaned views and templates

--- cookbook/views/update_views/update_quantified_ingredients.py
+++ cookbook/views/update_views/update_quantified_ingredients.py
@@ -21,6 +21,8 @@ def UpdateQuantifiedIngredientsView(request, pk: int):
         if update_quantified_ingredient_form.is_valid():
             update_quantified_ingredient_form.save()
             return redirect("cookbook:cookbook")
+        print(update_quantified_ingredient_form.errors)
+        print(update_quantified_ingredient_form.non_form_errors)
 
     context = {
         "formset": update_quantified_ingredient_form,
