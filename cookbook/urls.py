b072f445ba2a2df2bdd04f083dc36f87cba6d643
bogdan <bogdan.aldea0623@gmail.com>
2022-02-14 10:59:03 +0200

    added url pattern for recipe detail

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views.create_views import add_recipe_to_cookbook, add_instructions, add_quantified_ingredients
 from .views.list_views import cookbook
 from .views.update_views import update_recipe_view, update_instructions, update_quantified_ingredients
+from .views.detail_view import recipe
 
 app_name = 'cookbook'
 
 urlpatterns: list = [
     path("", cookbook.CookbookView, name="cookbook"),
+    path("recipe/<int:pk>/", recipe.RecipeDetailView, name="recipe_detail"),
     path("add-new-recipe/", add_recipe_to_cookbook.AddRecipeToCookbookView, name="add-new-recipe"),
     path("add-instructions/", add_instructions.AddInstructionToRecipeView, name="add-instructions"),
     path("add-ingredients/", add_quantified_ingredients.AddQuantifiedIngredientToRecipeView, name="add-ingredients"),
